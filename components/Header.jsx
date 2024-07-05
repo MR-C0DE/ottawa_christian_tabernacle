2d1e07246b35b5edc4086fa948b395152e517a32
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2024-07-05 02:49:49 -0400

    Menu et ajustement

--- components/Header.jsx
+++ components/Header.jsx
@@ -51,7 +51,6 @@ const Header = () => {
 
       {width <= 900 && (
       <div className={styles.Mini}>
-      <Languages />
         <Menu />
       </div>
       )}
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Link from "next/link";
-import { FaBars, FaTimes } from 'react-icons/fa'; // Importation des icônes de la bibliothèque react-icons
+import { FaBars, FaTimes } from "react-icons/fa"; // Importation des icônes de la bibliothèque react-icons
 import { useTranslation } from "next-i18next";
 import styles from "./stylesheets/Menu.module.css";
+import Languages from "./Languages";
 
 const Menu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,26 +15,45 @@ const Menu = () => {
 
   return (
     <div className={styles.Menu}>
-      <div onClick={toggleMenu} className={styles.MenuIcon}>
-        {menuOpen ? <FaTimes /> : <FaBars />} {/* Utilisation d'icônes pour ouvrir et fermer le menu */}
-      </div>
+      <div onClick={toggleMenu} className={styles.MenuIcon} >
+        {!menuOpen && <FaBars />}{" "}
+        {/* Utilisation d'icônes pour ouvrir et fermer le menu */}
+      </div> 
+
       {menuOpen && (
-        <nav className={styles.MenuNav}>
-          <ul className={styles.MenuList}>
-            <li className={styles.MenuItem}>
-              <Link href={"/"}>{t("Home")}</Link>
-            </li>
-            <li className={styles.MenuItem}>
-              <Link href={"/services"}>{t("Services")}</Link>
-            </li>
-            <li className={styles.MenuItem}>
-              <Link href={"/history"}>{t("History")}</Link>
-            </li>
-            <li className={styles.MenuItem}>
-              <Link href={"/contact"}>{t("Contact Us")}</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className={styles.Menu_Content}>
+          <div onClick={toggleMenu} className={styles.MenuIcon +" "+ styles.FaTimes} >
+            {menuOpen && <FaTimes />}{" "}
+            {/* Utilisation d'icônes pour ouvrir et fermer le menu */}
+          </div>
+          <nav className={styles.MenuNav}>
+            <ul className={styles.MenuList}>
+              <li className={styles.MenuItem}>
+                <Link href={"/"}>{t("Home")}</Link>
+              </li>
+              <li className={styles.MenuItem}>
+                {" "}
+                <Link href={"/visitus"}>Visit Us</Link>
+              </li>
+              <li className={styles.MenuItem}>
+                <Link href={"/sermons"}>Sermons</Link>
+              </li>
+              <li className={styles.MenuItem}>
+                <Link href={"/gallery"}>Gallery</Link>
+              </li>
+
+              <li className={styles.MenuItem}>
+                <Link href={"/give"}>Give</Link>
+              </li>
+              <li className={styles.MenuItem}>
+                <Link href={"/about"}>About Us</Link>
+              </li>
+              <li className={styles.MenuItemBtn}>
+                <Languages />
+              </li>
+            </ul>
+          </nav>
+        </div>
       )}
     </div>
   );
